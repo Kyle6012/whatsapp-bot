95fe2d94700d9de814e831deda972adebbe23386
Meshack Bahati <102649537+Kyle6012@users.noreply.github.com>
2024-06-29 16:39:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || 'Toxic').trim(),
+  SESSION_ID: (process.env.SESSION_ID || '29_6_9b43_15c9_7894').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
